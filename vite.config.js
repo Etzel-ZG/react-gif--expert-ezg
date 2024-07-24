69a18aeda3261be2f2922a42e9dcc539939794b7
Etzel ZG <etzelzapatag@gmail.com>
2024-07-24 17:10:13 -0500

    Descripción de los cambios

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-gif--expert-ezg/', // Cambia esto a tu ruta base en GitHub Pages
 });
